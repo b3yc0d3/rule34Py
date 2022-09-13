c867ab546ed4c873e6ce791ff29c32d2019edf0d
Tal A. Baskin <talbaskin.business@gmail.com>
2022-09-13 06:41:00 -0700

    Update api_urls.py
    Fixes issue #4 Tested Locally.

--- rule34Py/api_urls.py
+++ rule34Py/api_urls.py
@@ -7,7 +7,7 @@ class API_URLS(str, Enum):
     USER_FAVORITES = f"{__api_url__}index.php?page=favorites&s=view&id={{USR_ID}}" # returns: HTML
     GET_POST = f"{__api_url__}index.php?page=dapi&s=post&q=index&id={{POST_ID}}&json=1" # returns: JSON
     ICAME = f"{__base_url__}index.php?page=icame" # returns: HTML
-    RANDOM_POST = f"{__api_url__}index.php?page=post&s=random" #  returns: HTML
+    RANDOM_POST = f"{__base_url__}index.php?page=post&s=random" #  returns: HTML
     USER_PAGE = f"{__api_url__}index.php?page=account&s=profile&id={{USER_ID}}" # returns: HTML
     POOL = f"{__api_url__}index.php?page=pool&s=show&id={{POOL_ID}}" # returns: HTML
     TOPMAP = f"{__base_url__}index.php?page=toptags"
